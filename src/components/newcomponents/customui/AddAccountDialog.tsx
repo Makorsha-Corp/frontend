16a03cb3afaa7ad8b70c0259a167b2f614f3a6c9
Shohan Chowdhury <79315404+shohanchowdhury@users.noreply.github.com>
2026-03-31 23:41:49 +0600

    Test push

--- src/components/newcomponents/customui/AddAccountDialog.tsx
+++ src/components/newcomponents/customui/AddAccountDialog.tsx
@@ -19,7 +19,7 @@ import { Loader2 } from 'lucide-react';
 interface AddAccountDialogProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
-  defaultTagCode?: string;  // Pre-select tag when adding from a tab (supplier, vendor, client, etc.)
+  defaultTagCode?: string;  // Pre-select tag when adding from a tab (supplier, vendor, client, etc..)
 }
 
 const AddAccountDialog: React.FC<AddAccountDialogProps> = ({
